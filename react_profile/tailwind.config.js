59ce7af463a1046f16b92fc8043cfe5f0f0a6b65
amirkhanhard <ak9789858@gmail.com>
2025-04-16 21:14:40 +0530

    init

---
+++ react_profile/tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+export default {
+    darkMode: 'class', // 👈 this is important
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: { extend: {} },
+    plugins: [],
+  }
+  
